32cec08b35f5ecb2db9cb91b0cc2221a2751b94f
Santiago Nule <sj.nule@gmail.com>
2025-06-15 16:48:46 -0400

    Move build files to root for GitHub Pages

---
+++ santinule-portfolio/precache-manifest.28075cdf5ce127c4e55e3933d9c4903c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9f74e87fca44c5a1a406a9011a3202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "118629c9ee09b99a9b02",
+    "url": "/static/css/main.5cb41cfd.chunk.css"
+  },
+  {
+    "revision": "05b7d592060c685accd6",
+    "url": "/static/js/2.52da3b21.chunk.js"
+  },
+  {
+    "revision": "ccfbd15f903dbb6de863195ce52b790a",
+    "url": "/static/js/2.52da3b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed4b9ca329e02d7a7f0",
+    "url": "/static/js/3.97a74702.chunk.js"
+  },
+  {
+    "revision": "118629c9ee09b99a9b02",
+    "url": "/static/js/main.32fd306c.chunk.js"
+  },
+  {
+    "revision": "5e91481ba120150e693e",
+    "url": "/static/js/runtime-main.f1cc2d2f.js"
+  },
+  {
+    "revision": "5f50e098fa31387af75cc583cd3c0ef4",
+    "url": "/static/media/Random_walk.5f50e098.jpg"
+  },
+  {
+    "revision": "015dffb769ab6850737d20843bbd5784",
+    "url": "/static/media/TSDS.015dffb7.jpg"
+  },
+  {
+    "revision": "920daff9add96ffe640e992aac1b9fef",
+    "url": "/static/media/home_pic.920daff9.png"
+  },
+  {
+    "revision": "8ae5f0d6638c62b16c444e177950c4fc",
+    "url": "/static/media/hownottobewrong.8ae5f0d6.jpg"
+  },
+  {
+    "revision": "f8ef885873f6ba04db5a416fd4657ef1",
+    "url": "/static/media/sapiens.f8ef8858.jpg"
+  },
+  {
+    "revision": "f9bb7087dba91663b7c980872d824a73",
+    "url": "/static/media/sssaw.f9bb7087.jpg"
+  },
+  {
+    "revision": "0ba29c444847e3bbceb119bec157273f",
+    "url": "/static/media/zero_to_one.0ba29c44.jpg"
+  }
+]);
