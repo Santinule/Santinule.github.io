f2645f5f570af3ed7c08f434ec41eb1f28adeb5e
Santiago Nule <sj.nule@gmail.com>
2026-02-22 21:37:07 -0500

    added ClickSpark.js

--- santinule-portfolio/src/App.js
+++ santinule-portfolio/src/App.js
@@ -5,24 +5,33 @@ import Home from './pages/Home';
 import ReadingList from './pages/ReadingList';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import ClickSpark from './components/ClickSpark';
 
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        {/* Navigation component */}
-        <Navbar />
+      <ClickSpark
+        sparkColor='#fff'
+        sparkSize={10}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={400}
+      >
+        <div className="app-container">
+          {/* Navigation component */}
+          <Navbar />
 
-        {/* Main Content Area */}
-        <div className="main-content">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/reading-list" component={ReadingList} />
-            <Route path="/contact" component={Contact} />
-          </Switch>
+          {/* Main Content Area */}
+          <div className="main-content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/projects" component={Projects} />
+              <Route path="/reading-list" component={ReadingList} />
+              <Route path="/contact" component={Contact} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </ClickSpark>
     </Router>
   );
 }
